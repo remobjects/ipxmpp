143d3eea9b38ba33251c5d2513b4dde040f17a57
Carlo Kok <ck@remobjects.com>
2011-05-24 21:30:05 +0200

    Updated data

--- Elements/Message.cs
+++ Elements/Message.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace RemObjects.InternetPack.XMPP.Elements
 {
 
+    public enum MessageType { message, groupchat }
 
     public class Message : Stanza
     {
@@ -16,6 +17,51 @@ namespace RemObjects.InternetPack.XMPP.Elements
             Name = "message";
         }
 
+        public string Body
+        {
+            get
+            {
+                Element el = FindElement(Namespaces.ClientStreamNamespace, "body");
+                if (el != null)
+                    return el.Text;
+                return null;
+            }
+            set
+            {
+                Element el = FindElement(Namespaces.ClientStreamNamespace, "body");
+                if (el != null)
+                    el.Text = value;
+                else
+                {
+                    el = new UnknownElement();
+                    el.Parent = this;
+                    el.Name = "body";
+                    el.NamespaceURI = Namespaces.ClientStreamNamespace;
+                    el.Text = value;
+                    Elements.Add(el);
+                }
+            }
+        }
+
+        public MessageType MessageType
+        {
+            get
+            {
+                string s = GetAttributeByName(null, "type");
+                if (s == "groupchat")
+                    return XMPP.Elements.MessageType.groupchat;
+                return XMPP.Elements.MessageType.message;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case XMPP.Elements.MessageType.groupchat: AddOrReplaceAttribute(null, "type", "groupchat"); break;
+                    default: 
+                        AddOrReplaceAttribute(null, "type", "message"); break;
+                }
+            }
+        }
 
         public override ElementType Type
         {
